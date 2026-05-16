b905cb7b63158d6e9e7869b21cfc27a6f5052232
Meower-Pro <drvailshery@gmail.com>
2026-05-16 12:01:42 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp> // Includes the main menu files
+#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
-// This "$modify" tells Geode you are changing the Main Menu
 class $modify(MenuLayer) {
-    
     bool init() {
-        // 1. Run the game's normal code first
-        if (!MenuLayer::init()) return false;
+        if (!MenuLayer::init()) {
+            return false;
+        }
 
-        // 2. PUT YOUR CUSTOM C++ CODE HERE!
-        log::info("This message prints when the menu loads!");
-
+        log::info("Hello World from my first C++ mod!");
         return true;
     }
 };
